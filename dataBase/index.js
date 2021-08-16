d8b09235de64d109ffa1944be357d15d08f2e45d
Santiago Vera Nicola <sveranicola12@gmail.com>
2021-08-16 14:13:51 -0400

    added pg and connection to server

---
+++ dataBase/index.js
@@ -0,0 +1,16 @@
+const { Pool, Client } = require('pg');
+
+const client = new Client({
+  host: 'localhost',
+  port: 5432,
+  database: 'q_and_a'
+})
+
+
+client.on('error', (err, client) => {
+  console.log(err)
+});
+
+client.connect();
+
+module.exports = client;
